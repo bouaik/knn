5b9e69e211b40be5f0d648ec357b5f5c8b9d8d0d
bouaik <lhoussaine.bouaik@um6p.ma>
2024-01-15 23:32:39 +0100

    rsults to a text file

--- knn.rb
+++ knn.rb
@@ -2,30 +2,10 @@ require "zlib"
 
 require 'csv'
 
-train_data = CSV.read("train.csv")
+train_data = CSV.read("train_small.csv")
 
-test_data = "Some People Not Eligible to Get in on Google IPO, Google has billed its IPO as a way for everyday people to get in on the process, denying Wall Street the usual stranglehold it's had on IPOs. Public bidding, a minimum of just five shares, an open process with 28 underwriters - all this pointed to a new level of public participation. But this isn't the case."
+test_data = CSV.read("test.csv")
 
-ncds = []
-
-
-train_data.each_with_index do |t, i|
-  # x is train
-  # y is test
-  next if i == 0
-  train_line = t[1] + t[2]
-  cxy = Zlib::Deflate.deflate(train_line + " " + test_data)
-  cx = Zlib::Deflate.deflate(train_line)
-  cy = Zlib::Deflate.deflate(test_data)
-
-  ncd = (cxy.length - [cx, cy].min.length) / [cx, cy].max.length.to_f
-
-  ncds << [ncd, t[0]]
-end
-
-
-
-ncds = ncds.sort_by(&:first)
 
 def predict_class(a)
   freq =  a.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
@@ -33,5 +13,48 @@ def predict_class(a)
   predected =  a.max_by { |v| freq[v] }
   return predected[1]
 end
+success = 0
 
-puts predict_class(ncds[0..2])
+
+starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+test_data.each_with_index do |e, j|
+  next if j == 0
+  test_line = e[1] + e[2]
+
+  ncds = []
+
+
+
+  train_data.each_with_index do |t, i|
+    # x is train
+    # y is test
+    next if i == 0
+    train_line = t[1] + t[2]
+    cxy = Zlib::Deflate.deflate(train_line + " " + test_line)
+    cx = Zlib::Deflate.deflate(train_line)
+    cy = Zlib::Deflate.deflate(test_line)
+
+    ncd = (cxy.length - [cx, cy].min.length) / [cx, cy].max.length.to_f
+
+    ncds << [ncd, t[0]]
+  end
+
+  ncds = ncds.sort_by(&:first)
+
+
+  success += 1 if predict_class(ncds[0..2]) == e[0]
+
+end
+
+ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+elapsed = ending - starting
+
+
+text =  "Success is #{success} over #{test_data.length - 1} \nSuccess rate is #{success / (test_data.length - 1).to_f} \nElapsed time is #{elapsed}"
+
+
+out_file = File.new("results.txt", "w")
+out_file.puts(text)
+out_file.close
